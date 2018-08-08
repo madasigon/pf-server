e1064770f013b616f00f7d3fb09bca3565561375
Kiss Gergely <gergely.kiss@lynxanalytics.com>
2018-08-08 19:40:17 +0200

    working db experiment

--- WebApp/model.py
+++ WebApp/model.py
@@ -1,11 +1,13 @@
 
-User = None
 def register(db):
     global User
+
     class User(db.Model):
         id = db.Column(db.Integer, primary_key=True)
-        username = db.Column(db.String(80), unique=True, nullable=False)
-        email = db.Column(db.String(120), unique=True, nullable=False)
+        username = db.Column(db.String(80), unique=False, nullable=False)
+        email = db.Column(db.String(120), unique=False, nullable=False)
 
         def __repr__(self):
             return '<User %r>' % self.username
+
+            
--- experiment.py
+++ experiment.py
@@ -1,6 +1,8 @@
-from flask import redirect
-
+from flask import redirect, Markup, escape
+from WebApp import db
+from WebApp.model import User
 def app_modify(app):
+    
     @app.route("/pic")
     def pic():
         return redirect("https://pfinder.nyc3.digitaloceanspaces.com/doge.jpg")
@@ -12,3 +14,9 @@ def app_modify(app):
     @app.route("/pac")
     def pac():
         return redirect("/pic")
+    
+    @app.route("/error")
+    def err():
+        db.session.add(User(username="g",email="hali"))
+        db.session.commit()
+        return escape(User.query.all())
