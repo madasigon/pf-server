4af94955b52b6dab09699a2ba2c81cea5f2ca7af
Kiss Gergely <gergely.kiss@lynxanalytics.com>
2018-08-08 18:12:31 +0200

    Redrect experimentation

--- experiment.py
+++ experiment.py
@@ -1,3 +1,14 @@
+from flask import redirect
 
 def app_modify(app):
-    pass
+    @app.route("/pic")
+    def pic():
+        return redirect("https://pfinder.nyc3.digitaloceanspaces.com/doge.jpg")
+    
+    @app.route("/")
+    def index():
+        return '<img src="/pac"></img>'
+    
+    @app.route("/pac")
+    def pac():
+        return redirect("/pic")
