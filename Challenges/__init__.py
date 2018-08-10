7fda15b20c9d77a2a659afd33e731a36dbcebc62
Kiss Gergely <gergely.kiss@lynxanalytics.com>
2018-08-10 13:51:39 +0200

    set up challenge template folder

--- Challenges/__init__.py
+++ Challenges/__init__.py
@@ -1,14 +1,21 @@
 from jinja2 import Template
-from WebApp.space import static_storage, copy_from
+from WebApp.space import static_storage, copy_from, write_content
+
+from jinja2 import Environment, PackageLoader, select_autoescape
+env = Environment(
+    loader=PackageLoader('Challenges', 'templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
 
 def generate_challenges():
     return map(Challenge, range(1,11))
 
+
 class Challenge(object):
 
     def get_html(self):
-        doge = static_storage.create_file(copy_from("doge.jpg"))
-        return Template("<h1>{{ num }}*2</h1><br><img src=\"{{ doge }}\"></img>").render(num=self.num, doge=doge)
+        doge = static_storage.create_file(copy_from("doge.jpg"), ext="jpg")
+        return env.get_template("example.jinja2").render(num=self.num, doge=doge)
     
 
     def get_solution(self):
