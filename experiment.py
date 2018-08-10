003a8b8f48daba96ffb47291e50b0e1fd81a2ebb
Kiss Gergely <gergely.kiss@lynxanalytics.com>
2018-08-10 14:10:57 +0200

    minor update

--- experiment.py
+++ experiment.py
@@ -1,4 +1,4 @@
-from flask import redirect, Markup, escape, send_from_directory, render_template, jsonify
+from flask import redirect, Markup, escape, send_from_directory, render_template, jsonify, url_for
 from inspect import getsource
 from WebApp import db
 import WebApp.model as models
@@ -6,4 +6,7 @@ from WebApp.prepare import hash_function
 def app_modify(app):
     @app.route("/chs")
     def get_all():
-        return render_template("chs.jinja2", hf=getsource(hash_function), urls=str([ch.url for ch in models.Challenge.query.all()]))
+        return render_template("chs.jinja2", hf=getsource(hash_function), urls=str([url_for("challenge.challenge",hash=ch.url) for ch in models.Challenge.query.all()]))
+    @app.route("/error")
+    def error():
+        errorfdsf
