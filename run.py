58e379cef76994d0722999638f94704112625950
Kiss Gergely <gergely.kiss@lynxanalytics.com>
2018-08-10 12:10:38 +0200

    very minor run.py update

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ try:
     from experiment import app_modify
     app_modify(app)
 except ImportError:
-    pass
+    print("WARNING: COULD NOT LOAD EXPERIMENT.PY")
 
 prepare(app)
 app.run(debug=True)
