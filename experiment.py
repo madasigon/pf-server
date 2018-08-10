5bc5463d785d4db402a198b6ead29f85493b7fcc
Kiss Gergely <gergely.kiss@lynxanalytics.com>
2018-08-10 12:09:46 +0200

    update /chs to show hash function and challenge urls

--- experiment.py
+++ experiment.py
@@ -1,9 +1,9 @@
-from flask import redirect, Markup, escape, send_from_directory, render_template
+from flask import redirect, Markup, escape, send_from_directory, render_template, jsonify
+from inspect import getsource
 from WebApp import db
 import WebApp.model as models
-from WebApp.space import chpath
+from WebApp.prepare import hash_function
 def app_modify(app):
-    
     @app.route("/chs")
     def get_all():
-        return escape(str([ch.url for ch in models.Challenge.query.all()]))
+        return render_template("chs.jinja2", hf=getsource(hash_function), urls=str([ch.url for ch in models.Challenge.query.all()]))
